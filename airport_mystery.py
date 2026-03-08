32d93a47b8691355418f4e5f86687c3e8420bf48
ahmad <ahmaddchaudhary@gmail.com>
2026-03-08 23:55:38 +0200

    Step 7: Added rounds system and fuel/time mechanics

--- airport_mystery.py
+++ airport_mystery.py
@@ -31,9 +31,42 @@ LIMIT 20
 
 airports = cursor.fetchall()
 
-options = random.sample(airports, 4)
+rounds = 5
+fuel = 20
+time_left = 20
 
-print("\nPossible airports to investigate:\n")
+for round_number in range(1, rounds + 1):
 
-for i, airport in enumerate(options):
-    print(i+1, "-", airport[0], "(", airport[1], ",", airport[2], ")")
+    print("\n----------------------------")
+    print("Round", round_number)
+    print("Fuel:", fuel, "| Time:", time_left)
+    print("----------------------------")
+
+    options = random.sample(airports, 4)
+    criminal_airport = random.choice(options)
+
+    print("\nPossible airports to investigate:\n")
+
+    for i, airport in enumerate(options):
+        print(i+1, "-", airport[0], "(", airport[1], ",", airport[2], ")")
+
+    choice = int(input("\nChoose an airport to investigate (1-4): "))
+
+    selected_airport = options[choice - 1]
+
+    print("\nYou chose to investigate:")
+    print(selected_airport[0], "in", selected_airport[1], ",", selected_airport[2])
+
+    if selected_airport == criminal_airport:
+        print("\nYou found the criminal! Mission success.")
+        fuel -= 1
+        time_left -= 1
+    else:
+        print("\nWrong lead. The criminal escaped this airport.")
+        fuel -= 2
+        time_left -= 2
+
+    if fuel <= 0 or time_left <= 0:
+        print("\nYou ran out of resources.")
+        print("The Phantom Traveler escaped.")
+        break
